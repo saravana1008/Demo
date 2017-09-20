92de725858055075441cee9e85c337f0e960d87d
APPLE <apple@Saravanas-Mac-mini.local>
2017-09-20 11:56:36 +0530

    Commit from dev branch.

--- sampleProject/ViewController.swift
+++ sampleProject/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let stringConstant = "Changes in develop"
+        print(stringConstant)
     }
 
     override func didReceiveMemoryWarning() {
