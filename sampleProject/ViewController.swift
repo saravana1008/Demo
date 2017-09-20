b2e7461c08df0a5fe072fa0c2297f14418eaaff3
APPLE <apple@Saravanas-Mac-mini.local>
2017-09-20 11:59:56 +0530

    resolved merge conflicts.

--- sampleProject/ViewController.swift
+++ sampleProject/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let stringConstant = "Changes in develop"
+        let stringConstant = "testing purpose"
         print(stringConstant)
     }
 
